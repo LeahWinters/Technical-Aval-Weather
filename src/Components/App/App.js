57b83ef89b98fd151cc3b2f4b7bb79f1480bea03
Leah Winters <leah.winters24@gmail.com>
2020-10-05 11:45:05 -0400

    Remove code not needed in App component and changed file path for better organization

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect }from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>My weather app</p>
+    </div>
+  );
+}
+
+export default App;
